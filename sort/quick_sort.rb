5d70ab315afb6acdd40be8bf1b012765734f5372
Stanley Chan <stanley.chan@autodesk.com>
2019-05-29 00:59:09 +0800

    Added quick sort

---
+++ sort/quick_sort.rb
@@ -0,0 +1,70 @@
+# The left pointer continuously moves one cell to the right until it reaches a value that is greater than or equal to the pivot, and then stops.
+#
+# Then, the right pointer continuously moves one cell to the left until it reaches a value that is less than or equal to the pivot, and then stops.
+#
+# We swap the values that the left and right pointers are pointing to.
+#
+# We continue this process until the pointers are pointing to the very same value or the left pointer has moved to the right of the right pointer.
+#
+# Finally, we swap the pivot with the value that the left pointer is currently pointing to.
+class SortableArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def partition!(left_pointer, right_pointer)
+    return if right_pointer - left_pointer <= 0
+
+
+    # We always choose the right most element as the pivot
+    pivot_position = right_pointer
+    pivot = @array[pivot_position]
+    # Exclude the pivot from the comparisons
+    right_pointer -= 1
+    loop do
+      count = 0
+      if @array[left_pointer] <= pivot
+        left_pointer += 1
+      elsif @array[right_pointer] >= pivot
+        right_pointer -= 1
+      else
+        swap(left_pointer, right_pointer)
+      end
+      if left_pointer >= right_pointer
+        # Don't swap if the pointer value is actually bigger than the left_pointer value
+        break if @array[left_pointer] <= pivot
+
+        swap(left_pointer, pivot_position)
+        break
+      end
+      count += 1
+      puts "left_pointer: #{left_pointer}, right_pointer: #{right_pointer}"
+      puts "loop #{count}: #{@array.to_s}"
+    end
+    puts "After swapping left_pointer: #{left_pointer} right_pointer: #{right_pointer}"
+    partition!(0, pivot_position - 1)
+    partition!(pivot_position + 1, right_pointer)
+  end
+
+  def swap(pointer_1, pointer_2)
+    temp_value = @array[pointer_1]
+    @array[pointer_1] = @array[pointer_2]
+    @array[pointer_2] = temp_value
+  end
+
+  def quick_sort
+    puts "Starting position #{@array.to_s}"
+    partition!(0, (@array.size - 1))
+    puts "Ending position #{@array.to_s}"
+  end
+end
+
+
+ar = [3,2,1,4,5]
+
+sortable_array = SortableArray.new(ar)
+sortable_array.quick_sort
+
+
--- sort/selection_sort.rb
+++ sort/selection_sort.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Complexity is about O(n^2)
 # Calculation
 # n + (n-1) + (n-2).. --> n-n = Total Comparisons needed for each element
@@ -7,12 +9,10 @@
 # Gist of how this works - Remember the index of the lowest number of each iterations, and
 # at the end of each iteration, move the lowest number to the front.
 def selection_sort(ar)
-  (0..(ar.size-1)).each do |i|
+  (0..(ar.size - 1)).each do |i|
     smallest = 0
-    (i..(ar.size-1)).each do |j|
-      if ar[j] > ar[smallest]
-        smallest = j
-      end
+    (i..(ar.size - 1)).each do |j|
+      smallest = j if ar[j] > ar[smallest]
       temp = ar[j]
       ar[j] = ar[smallest]
       ar[smallest] = temp
@@ -20,7 +20,6 @@ def selection_sort(ar)
   end
   ar
 end
-
 
 array = [65, 55, 45, 35, 25, 15, 10]
 puts selection_sort(array).to_s
