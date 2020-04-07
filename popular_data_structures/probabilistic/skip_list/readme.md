a127828e3f4252acd5275051c9dff9102d3e2ed8
Stanley Chan <stanley.chan@autodesk.com>
2020-04-07 21:09:09 +0800

    add skip list

---
+++ popular_data_structures/probabilistic/skip_list/readme.md
@@ -0,0 +1,7 @@
+- SkipList is great for multi threaded environments where the value can be 
+accessed without being blocked.
+- Inserts can be done dynamically without re-referencing multiples nodes such as the case when balancing trees 
+- Skiplist by default is sorted. 
+- When there is a need of using Binary Search Trees, prefer skip list. 
+
+![Skip List Animation](./skip_list_animation.gif)
---
+++ popular_data_structures/probabilistic/skip_list/skip_list.rb
@@ -0,0 +1,197 @@
+# Credits to: https://github.com/light24bulbs/dynamic-skiplist/blob/master/lib/dskiplist.rb
+
+class DSkipList
+  attr_accessor :level
+  attr_accessor :header
+  include Enumerable
+
+  class Node
+    attr_accessor :key
+    attr_accessor :value
+    attr_accessor :forward
+
+    def initialize(k, v = nil)
+      @key = k
+      @value = v.nil? ? k : v
+      @forward = []
+    end
+  end
+
+  def initialize(top_level = Float::INFINITY)
+    @header = Node.new(1)
+    @level = 0
+    @max_level = top_level
+    @p = 0.5
+    @node_nil = Node.new(1000000)
+  end
+
+  def clear
+    initialize(@max_level)
+    return self
+  end
+
+  #returns previous node if exact key match is not found
+  def find_node(search_key)
+    x = @header
+    @level.downto(0) do |i|
+      #puts "on level #{i}"
+      while x.forward[i] and x.forward[i].key < search_key
+        #puts "walked node #{x.key} on level #{i}"
+        x = x.forward[i]
+      end
+    end
+    x = x.forward[0] if x.forward[0].key == search_key
+    return x
+  end
+
+  def [] key
+    node = self.find_node(key)
+    if node and node.key == key
+      return node.value
+    else
+      return nil
+    end
+  end
+
+  def random_level
+    v = 0
+    while rand < @p && v < @max_level
+      v += 1
+    end
+    v
+  end
+
+  def delete(search_key)
+    update = []
+    x = @header
+    @level.downto(0) do |i|
+      while x.forward[i] and x.forward[i].key < search_key
+        x = x.forward[i]
+      end
+      update[i] = x
+    end
+    x = x.forward[0]
+    if x and x.key == search_key
+      0.upto(x.forward.length - 1) do |i|
+        update[i].forward[i] = x.forward[i]
+      end
+      return true
+    else
+      return false
+    end
+  end
+
+
+  def insert(search_key, new_value = nil)
+    new_value = search_key if new_value.nil?
+    update = []
+    x = @header
+    @level.downto(0) do |i|
+      while x.forward[i] and x.forward[i].key < search_key
+        x = x.forward[i]
+      end
+      update[i] = x
+    end
+    x = x.forward[0]
+    if x and x.key == search_key
+      x.value = new_value
+    else
+      v = random_level
+      if v > @level
+        (@level + 1).upto(v) do |i|
+          update[i] = @header
+        end
+        @level = v
+      end
+      x = Node.new(search_key, new_value)
+      0.upto(v) do |i|
+        x.forward[i] = update[i].forward[i]
+        update[i].forward[i] = x
+      end
+    end
+    return self
+  end
+
+  def []= key, value
+    self.insert(key, value)
+  end
+
+  def insert_hash(hash)
+    hash.each {|key, value| self[key] = value}
+    return self
+  end
+
+  def count(from = nil, to = nil, level = 0)
+    walk(from, to, nil, level, nil)
+  end
+
+  def walk(from, to, limit, level, output)
+    if from
+      x = find_node(from)
+      x = x.forward[level] if x.forward[level]
+    else
+      x = @header.forward[level]
+    end
+    #if no block is given, assume we are trying to count and avoid the expensive yield below
+    if !block_given?
+      count = 0
+      while x
+        break if to && x.key >= to
+        count += 1
+        x = x.forward[level]
+      end
+      return count
+    elsif to or limit
+      count = 0
+      while !x.nil?
+        yield(x, output)
+        count += 1
+        break if to and x.key >= to
+        break if limit and count == limit
+        x = x.forward[level]
+      end
+    else
+      while x
+        yield(x, output)
+        x = x.forward[level]
+      end
+    end
+    return output
+  end
+
+  def to_h(from = nil, to = nil, limit = nil, level = 0)
+    walk(from, to, limit, level, {}) {|n, hash| hash[n.key] = n.value}
+  end
+
+  def to_a(from = nil, to = nil, limit = nil, level = 0)
+    walk(from, to, limit, level, []) {|n, arr| arr.push(n.value)}
+  end
+  alias_method :to_ary, :to_a
+
+  def largest
+    x = @header
+    @level.downto(0) do |i|
+      while x.forward[i]
+        x = x.forward[i]
+      end
+    end
+    return x.value
+  end
+
+  def smallest
+    return @header.forward[0].value
+  end
+
+  def to_s
+    str = ""
+    @level.downto(0) do |l|
+      str << "Level #{l}: " + to_a(nil, nil, nil, l).join('-') + "\n"
+    end
+    return str
+  end
+
+  def each(&block)
+    self.to_a.each(&block)
+  end
+
+end
